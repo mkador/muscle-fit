6bfb4b79911be54ea7f3f71eeca351406840d097
Mk Adore <mkador169@gmil.com>
2022-09-29 14:16:15 +0600

    Toast added

--- src/Components/Activities/TimeCalculate/TimeCalculate.js
+++ src/Components/Activities/TimeCalculate/TimeCalculate.js
@@ -1,5 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { addToDbBreakTimes,getStoredTimes } from '../../../utilities/fakedb';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const notify = () => {
+    toast.success('Done!! The Process,Thank You', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+};
+
 
 const TimeCalculate = ({exerciseTimes}) => {
     const [time,setTime] = useState(0);
@@ -22,7 +37,7 @@ const TimeCalculate = ({exerciseTimes}) => {
                         <img className='my-3 mx-auto d-block w-50 rounded-4' src="./image/picture-7.jpg" alt="" />
                        
                     </div>
-                    <h4 className=' mb-2 text-center'>Md. Musa Kalimulla</h4>
+                    <h4 className='font-monospace mb-2 text-center'>Md. Musa Kalimulla</h4>
                     <div className='d-flex justify-content-between align-items-center'>
                         <button className='btn btn-outline-none bg-danger text-white rounded-2'><h6>Weight: 64kg</h6></button>
                         <button className='btn btn-outline-none bg-danger text-white rounded-2'><h6>Height: 170cm</h6></button>
@@ -31,7 +46,7 @@ const TimeCalculate = ({exerciseTimes}) => {
                        
                     </div>
                     <div>
-                        <h5 className='mt-3 text-center'>Add a Break</h5>
+                        <h5 className='mt-3 text-center font-monospace'>Add a Break</h5>
                         <div className='d-flex justify-content-between align-items-center'>
                         {
                             breakTimes.map(breakTime=> <button onClick={()=>addBreakTimes(breakTime)}  type="button" className="rounded-5 btn btn-outline-danger">{breakTime}</button>)
@@ -40,15 +55,16 @@ const TimeCalculate = ({exerciseTimes}) => {
                         
                         </div>
                     </div>
-                    <h5 className='mt-3 text-center'>Exercise Details</h5>
+                    <h5 className='mt-3 text-center font-monospace'>Exercise Details</h5>
                     <div>
-                    <p className='p-2 bg-danger  rounded-5 text-center'>Exercise Time: {exerciseTimes} Second </p>
-                    <p className='p-2 bg-danger rounded-5 text-center'>Break Time: {storedBreakTimes}  </p>
+                    <p className='p-2 bg-danger  rounded-5 text-center font-monospace text-light'>Exercise Time: {exerciseTimes} Second </p>
+                    <p className='p-2 bg-danger rounded-5 text-center font-monospace text-light'>Break Time: {storedBreakTimes}  </p>
                     </div>
                     <div className=' mt-3 text-center'>
-                    <button className='btn btn-success'>Activity Completed</button>
+                    <button onClick={notify} className='btn btn-success font-monospace'>Activity Completed</button>
                     </div>
                     </div>
+                    <ToastContainer></ToastContainer>
         </div>
     );
 };
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <div>
             <nav>
                 <img className='rounded mx-auto d-block img-fluid' width="100" height="100" src={logo} alt="" />
-                <h3 className='text-center mt-2'>Muscle Fit</h3>
+                <h3 className='text-center mt-2'> <span className='text-success font-monospace'>Muscle</span> <span className='text-warning font-monospace'>Fit</span></h3>
             </nav>
         </div>
     );
